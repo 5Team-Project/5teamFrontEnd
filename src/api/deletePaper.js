37840465b9b8c1c78749c0b64067ee2ada519875
AdamSeungheonShin <ssh9809@naver.com>
2024-05-13 12:49:31 +0900

    페이지 삭제 시 리디렉션 구현

--- src/api/deletePaper.js
+++ src/api/deletePaper.js
@@ -2,8 +2,7 @@ import { instance } from './Axios';
 
 export const deletePaper = async (recipientId) => {
   try {
-    const res = await instance.delete(`/6-5/messages/${recipientId}/`);
-    return res.data;
+    const res = await instance.delete(`/6-5/recipients/${recipientId}/`);
   } catch (error) {
     console.error(error);
   }
--- src/components/postlist/MessageList.jsx
+++ src/components/postlist/MessageList.jsx
@@ -99,9 +99,15 @@ const MessageList = ({
   const themeContext = useContext(ThemeContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalData, setModalData] = useState({});
-  const navigate = useNavigate();
+
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [paperIdToDelete, setPaperIdToDelete] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleRedirectionToList = () => {
+    navigate('/list');
+  };
 
   const handleMessageClick = (data) => {
     if (!showDeleteButton) {
@@ -127,10 +133,9 @@ const MessageList = ({
   const handleDeletePaper = async () => {
     try {
       const res = await deletePaper(`${paperIdToDelete}`);
-      if (res) {
-        navigate('/list');
-        console.log('Paper deleted successfully!');
-      }
+      closeConfirmModal();
+      handleRedirectionToList();
+      console.log(`Paper No.${recipientId} has successfully deleted.`);
     } catch (e) {
       console.error(e);
     }
